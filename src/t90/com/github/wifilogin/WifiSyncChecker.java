5688450ca1a0f8b89a848cee7d6924d1de346ee7
Vladimir Vasiltsov <vasiltsov@gmail.com>
2012-11-30 13:52:46 -0500

    Pinging external server.

--- src/t90/com/github/wifilogin/WifiSyncChecker.java
+++ src/t90/com/github/wifilogin/WifiSyncChecker.java
@@ -14,6 +14,7 @@ import tinyq.Query;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
+import java.util.Calendar;
 
 /**
  * User: VasiltsV
@@ -28,20 +29,35 @@ public class WifiSyncChecker {
         WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
         WifiInfo connectionInfo;
         if(wifiManager != null && (connectionInfo = wifiManager.getConnectionInfo()) != null){
-            String ssid = connectionInfo.getSSID();
-            if(ssid != null && ssid.toLowerCase().equals("nguest")){
-                BufferedReader reader = null;
+            try{
+                String result;
+                HttpUtil httpUtil;
                 try{
-                    reader = new BufferedReader(new InputStreamReader(new FileInputStream("/storage/sdcard0/p.txt")));
-                    String wifiUserName = reader.readLine();
-                    String wifiPassword = reader.readLine();
-                    HttpUtil httpUtil = new HttpUtil("https://securelogin.arubanetowrks.com/auth/index.html/u", null, null);
-                    String result = httpUtil.get("https://securelogin.arubanetowrks.com/auth/index.html/u", new Query<String>(new String[]{"user=" + wifiUserName, "password=" + wifiPassword, "Login=I+ACCEPT"}));
+                    httpUtil = new HttpUtil("http://t90.dyndns.org/cgi-bin/dt", null, null);
+                    result = httpUtil.get("http://t90.dyndns.org/cgi-bin/dt",new Query<String>(new String[]{}));
+                    if(result != null && result.contains(Calendar.getInstance().get(Calendar.YEAR) + ".")){
+                        return;
+                    }
                 }
                 catch (Exception e){
-                    Log.e(TAG, Util.exceptionToString(e));
+                    Log.i(TAG,Util.exceptionToString(e));
+                }
+                String ssid = connectionInfo.getSSID();
+                if(ssid != null && ssid.toLowerCase().equals("nguest")){
+                    BufferedReader reader = null;
+
+                        reader = new BufferedReader(new InputStreamReader(new FileInputStream("/storage/sdcard0/p.txt")));
+                        String wifiUserName = reader.readLine();
+                        String wifiPassword = reader.readLine();
+                        httpUtil = new HttpUtil("https://securelogin.arubanetowrks.com/auth/index.html/u", null, null);
+
+                        result = httpUtil.get("https://securelogin.arubanetowrks.com/auth/index.html/u", new Query<String>(new String[]{"user=" + wifiUserName, "password=" + wifiPassword, "Login=I+ACCEPT"}));
                 }
             }
+            catch (Exception e){
+                Log.e(TAG, Util.exceptionToString(e));
+            }
+
         }
     }
 }
